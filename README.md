a1649a7b368c7a8d3c7f5230debce2e97de712ff
Arturo López <lgzarturo@gmail.com>
2022-11-10 00:24:43 -0500

    Configuración de babel y webpack

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ npm init -y
 npm install --save-dev --save-exact webpack webpack-cli
 npm install --save-exact react react-dom
 npm install --save-exact --save-dev @babel/core babel-loader @babel/preset-env @babel/preset-react
+npm install --save-exact --save-dev html-webpack-plugin
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   module: {
     rules: [
@@ -9,5 +11,11 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ]
 }
